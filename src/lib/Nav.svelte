d4e6992fa65ebcd5766eb44a986d4cc3ba2f9d22
Marcell Endrey <endrey.marcell@gmail.com>
2022-04-25 20:53:09 +0200

    draw the rest of the fucking owl

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import "../global.css";
+</script>
+
+<nav>
+  <img src="/img/coding-profile.jpg" alt="Marca coding" />
+  <h2>Marca's blog</h2>
+  <section>Hi, I am Marca and this is my blog.</section>
+  <ul>
+    <li>
+      <a href="/">Link 1</a>
+    </li>
+    <li>
+      <a href="/">Link 1</a>
+    </li>
+  </ul>
+</nav>
+
+<style>
+  nav {
+    text-align: center;
+    border-bottom: 1px solid black;
+  }
+
+  nav img {
+    max-width: 100%;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  @media screen and (min-width: 600px) {
+    nav {
+      padding: 1rem;
+      text-align: right;
+      border: none;
+    }
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,33 @@
 <script lang="ts">
-  import Header from "$lib/header/Header.svelte";
-  import "../app.css";
+  import Nav from "$lib/Nav.svelte";
+  import "../global.css";
 </script>
 
-<Header />
+<div id="container">
+  <Nav />
+  <main>
+    <slot />
+  </main>
+</div>
 
-<main>
-  <slot />
-</main>
-
-<footer>
-  <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+<footer />
 
 <style>
+  #container {
+    display: grid;
+    grid-template-columns: 1fr;
+    background: linear-gradient(to top right, rgb(249, 250, 245), rgb(251, 251, 250));
+    height: 100%;
+  }
+
   main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
     padding: 1rem;
-    width: 100%;
-    max-width: 1024px;
-    margin: 0 auto;
-    box-sizing: border-box;
+    max-width: 700px;
   }
 
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 40px 0;
+  @media screen and (min-width: 600px) {
+    #container {
+      grid-template-columns: minmax(200px, 300px) auto;
     }
   }
 </style>
