f89b7e43891bfc819b81c005a24e08ed77e7ac4a
Marcell Endrey <endrey.marcell@gmail.com>
2022-04-26 21:35:32 +0200

    fix static adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,15 @@ import preprocess from "svelte-preprocess";
 const config = {
   preprocess: preprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: null,
+      precompress: false,
+    }),
+    prerender: {
+      default: true,
+    },
   },
 };
 
