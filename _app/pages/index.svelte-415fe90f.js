cde3d645eb5ad2ddd82ac458383962048d4ff90c
endreymarcell <endrey.marcell@gmail.com>
2022-05-14 13:46:48 +0000

    Deploying to gh-pages from @ endreymarcell/namingthings@6e1e3cdc51edc0b181605ec3beed12baa8c86865 🚀

---
+++ _app/pages/index.svelte-415fe90f.js
@@ -0,0 +1,39 @@
+import{S as V,i as z,s as K,e as f,t as b,k as _,c as g,a as y,h as q,d as t,m as w,b as c,g as l,F as v,n as M,x as J,K as L,y as F,z as B,r as U,p as Y,C as A}from"../chunks/index-c2bd7bd8.js";function j(N){let s,o,u,n,i,r,m,I,S,p,k,P,d,$,x,h,D;return{c(){s=f("h2"),o=b("Just because you like it doesn't mean it's good for you"),u=_(),n=f("p"),i=b(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo nisl ac pellentesque sagittis. Suspendisse
+  dictum feugiat nunc, at posuere augue tincidunt eget. Suspendisse neque metus, faucibus eu lacus sed, laoreet feugiat
+  urna. In hac habitasse platea dictumst. Nunc eu faucibus mauris, sed dictum tellus. Pellentesque habitant morbi
+  tristique senectus et netus et malesuada fames ac turpis egestas. Nulla ac accumsan velit. Vestibulum purus magna,
+  iaculis ac maximus vel, rutrum nec nisl. Fusce ac sagittis nulla. Nulla eget elit massa. Donec interdum elit vitae
+  risus accumsan ultrices. Morbi venenatis purus sed eros pellentesque, a posuere ipsum imperdiet. Praesent sagittis
+  libero non congue feugiat.`),r=_(),m=f("p"),I=b(`Curabitur rutrum sollicitudin fringilla. Mauris eget ultricies turpis. Phasellus ultricies ante eu lectus aliquam,
+  rutrum fermentum dolor maximus. Vivamus pellentesque nisi erat, in consectetur dui suscipit a. Class aptent taciti
+  sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi enim metus, faucibus nec eleifend
+  dapibus, tristique eget eros. Nulla sed cursus elit, a ultricies erat. In hac habitasse platea dictumst. Nam efficitur
+  est nibh, eu semper velit porta a. Phasellus sit amet augue a nunc ullamcorper rutrum vel eu leo. Donec euismod sem
+  quis massa fringilla cursus. Suspendisse et convallis sapien, a porttitor ante. Sed sit amet nunc eu tortor molestie
+  pretium id sed quam. Mauris et commodo lacus, vitae vulputate felis. Duis porttitor tristique augue eu porta.
+  Phasellus in pharetra tortor.`),S=_(),p=f("p"),k=b("SvelteKit is a framework for building extremely high-performance web apps."),P=_(),d=f("p"),$=b(`Building an app with all the modern best practices is fiendishly complicated. Those practices include build
+  optimizations, so that you load only the minimal required code; offline support; prefetching pages before the user
+  initiates navigation; and configurable rendering that allows you to generate HTML on the server or in the browser at
+  runtime or at build-time. SvelteKit does all the boring stuff for you so that you can get on with the creative part.`),x=_(),h=f("p"),D=b(`It uses Vite with a Svelte plugin to provide a lightning-fast and feature-rich development experience with Hot Module
+  Replacement (HMR), where changes to your code are reflected in the browser instantly. You don't need to know Svelte to
+  understand the rest of this guide, but it will help. In short, it's a UI framework that compiles your components to
+  highly optimized vanilla JavaScript. Read the introduction to Svelte blog post and the Svelte tutorial to learn more.`),this.h()},l(e){s=g(e,"H2",{class:!0});var a=y(s);o=q(a,"Just because you like it doesn't mean it's good for you"),a.forEach(t),u=w(e),n=g(e,"P",{class:!0});var E=y(n);i=q(E,`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo nisl ac pellentesque sagittis. Suspendisse
+  dictum feugiat nunc, at posuere augue tincidunt eget. Suspendisse neque metus, faucibus eu lacus sed, laoreet feugiat
+  urna. In hac habitasse platea dictumst. Nunc eu faucibus mauris, sed dictum tellus. Pellentesque habitant morbi
+  tristique senectus et netus et malesuada fames ac turpis egestas. Nulla ac accumsan velit. Vestibulum purus magna,
+  iaculis ac maximus vel, rutrum nec nisl. Fusce ac sagittis nulla. Nulla eget elit massa. Donec interdum elit vitae
+  risus accumsan ultrices. Morbi venenatis purus sed eros pellentesque, a posuere ipsum imperdiet. Praesent sagittis
+  libero non congue feugiat.`),E.forEach(t),r=w(e),m=g(e,"P",{class:!0});var H=y(m);I=q(H,`Curabitur rutrum sollicitudin fringilla. Mauris eget ultricies turpis. Phasellus ultricies ante eu lectus aliquam,
+  rutrum fermentum dolor maximus. Vivamus pellentesque nisi erat, in consectetur dui suscipit a. Class aptent taciti
+  sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi enim metus, faucibus nec eleifend
+  dapibus, tristique eget eros. Nulla sed cursus elit, a ultricies erat. In hac habitasse platea dictumst. Nam efficitur
+  est nibh, eu semper velit porta a. Phasellus sit amet augue a nunc ullamcorper rutrum vel eu leo. Donec euismod sem
+  quis massa fringilla cursus. Suspendisse et convallis sapien, a porttitor ante. Sed sit amet nunc eu tortor molestie
+  pretium id sed quam. Mauris et commodo lacus, vitae vulputate felis. Duis porttitor tristique augue eu porta.
+  Phasellus in pharetra tortor.`),H.forEach(t),S=w(e),p=g(e,"P",{class:!0});var R=y(p);k=q(R,"SvelteKit is a framework for building extremely high-performance web apps."),R.forEach(t),P=w(e),d=g(e,"P",{class:!0});var C=y(d);$=q(C,`Building an app with all the modern best practices is fiendishly complicated. Those practices include build
+  optimizations, so that you load only the minimal required code; offline support; prefetching pages before the user
+  initiates navigation; and configurable rendering that allows you to generate HTML on the server or in the browser at
+  runtime or at build-time. SvelteKit does all the boring stuff for you so that you can get on with the creative part.`),C.forEach(t),x=w(e),h=g(e,"P",{class:!0});var T=y(h);D=q(T,`It uses Vite with a Svelte plugin to provide a lightning-fast and feature-rich development experience with Hot Module
+  Replacement (HMR), where changes to your code are reflected in the browser instantly. You don't need to know Svelte to
+  understand the rest of this guide, but it will help. In short, it's a UI framework that compiles your components to
+  highly optimized vanilla JavaScript. Read the introduction to Svelte blog post and the Svelte tutorial to learn more.`),T.forEach(t),this.h()},h(){c(s,"class","svelte-lhn2n5"),c(n,"class","svelte-lhn2n5"),c(m,"class","svelte-lhn2n5"),c(p,"class","svelte-lhn2n5"),c(d,"class","svelte-lhn2n5"),c(h,"class","svelte-lhn2n5")},m(e,a){l(e,s,a),v(s,o),l(e,u,a),l(e,n,a),v(n,i),l(e,r,a),l(e,m,a),v(m,I),l(e,S,a),l(e,p,a),v(p,k),l(e,P,a),l(e,d,a),v(d,$),l(e,x,a),l(e,h,a),v(h,D)},p:M,i:M,o:M,d(e){e&&t(s),e&&t(u),e&&t(n),e&&t(r),e&&t(m),e&&t(S),e&&t(p),e&&t(P),e&&t(d),e&&t(x),e&&t(h)}}}class G extends V{constructor(s){super(),z(this,s,null,j,K,{})}}function O(N){let s,o,u,n;return u=new G({}),{c(){s=f("meta"),o=_(),J(u.$$.fragment),this.h()},l(i){const r=L('[data-svelte="svelte-itytsq"]',document.head);s=g(r,"META",{name:!0,content:!0}),r.forEach(t),o=w(i),F(u.$$.fragment,i),this.h()},h(){document.title="Naming Things",c(s,"name","description"),c(s,"content","Naming things is hard: a blog about software engineering")},m(i,r){v(document.head,s),l(i,o,r),B(u,i,r),n=!0},p:M,i(i){n||(U(u.$$.fragment,i),n=!0)},o(i){Y(u.$$.fragment,i),n=!1},d(i){t(s),i&&t(o),A(u,i)}}}class W extends V{constructor(s){super(),z(this,s,null,O,K,{})}}export{W as default};
