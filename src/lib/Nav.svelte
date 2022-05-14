91bf6e9950abe7c13f1f5185fb79a40ddb185518
Marcell Endrey <endrey.marcell@gmail.com>
2022-05-14 17:54:53 +0200

    nicer

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -5,7 +5,7 @@
 <div id="header-container">
   <header>
     <div id="title-container">
-      <h1>Naming Things is Hard</h1>
+      <h1>Naming</h1><h1>Things</h1><h1>is Hard</h1>
     </div>
     <nav>
       <div>
@@ -38,9 +38,12 @@
     border-bottom-width: 0;
     border-style: solid;
     border-color: black;
+
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: 0;
+
     background-color: #DDD;
   }
 
@@ -63,10 +66,28 @@
   }
 
   @media screen and (min-width: 600px) {
+    #title-container {
+      flex-direction: column;
+      padding-block: 1rem;
+    }
+
+    h1 {
+      margin: 0.1rem;
+      font-size: 3.5rem;
+    }
+
     nav {
-      padding: 1rem;
-      text-align: right;
-      border: none;
+      height: initial;
+      font-size: 1.6rem;
+      flex-direction: column;
+      gap: 0.7rem;
+      padding-block: 1rem;
+    }
+
+    nav > div:hover {
+      text-decoration: underline;
+      text-decoration-thickness: 1px;
+      text-underline-offset: 5px;
     }
   }
 </style>
