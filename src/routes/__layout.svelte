ad50d3e5d31e995bd4d3c7673fa5b0e48df82fce
Marcell Endrey <endrey.marcell@gmail.com>
2022-07-30 17:10:45 +0200

    remove unused property

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <!-- This is the global layout file; it "wraps" every page on the site. (Or more accurately: is the parent component to every page component on the site.) -->
 <script context="module">
-  export const load = async ({ url, fetch }) => {
+  export const load = async ({ url }) => {
     return {
       props: {
         path: url.pathname,
