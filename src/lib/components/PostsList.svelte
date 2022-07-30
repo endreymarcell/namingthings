8b94d503bfd2d70c4053f7a0cab4c35d0b100cff
Marcell Endrey <endrey.marcell@gmail.com>
2022-07-30 11:27:05 +0200

    remove coverImage

--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -7,13 +7,6 @@
     <li>
       <article>
         <a href="/blog/{post.slug}">
-          <img
-            src={post.coverImage}
-            alt=""
-            width={post.coverWidth}
-            height={post.coverHeight}
-            style="ratio: {post.coverWidth} / {post.coverHeight}"
-          />
           <h2>
             {post.title}
           </h2>
--- src/routes/blog/[post].svelte
+++ src/routes/blog/[post].svelte
@@ -23,7 +23,7 @@
   export let PostContent;
   export let meta;
 
-  const { title, excerpt, date, updated, coverImage, coverWidth, coverHeight, categories } = meta;
+  const { title, excerpt, date, updated, categories } = meta;
 </script>
 
 <svelte:head>
@@ -36,22 +36,10 @@
   <meta property="og:description" content={excerpt} />
   <meta name="twitter:description" content={excerpt} />
   <!-- <meta property="og:image" content="https://yourdomain.com/image_path" /> -->
-  <meta property="og:image:width" content={coverWidth} />
-  <meta property="og:image:height" content={coverHeight} />
   <!-- <meta name="twitter:image" content="https://yourdomain.com/image_path" /> -->
 </svelte:head>
 
 <article class="post">
-  <!-- You might want to add an alt frontmatter attribute. If not, leaving alt blank here works, too. -->
-  <img
-    class="cover-image"
-    src={coverImage}
-    alt=""
-    style="aspect-ratio: {coverWidth} / {coverHeight};"
-    width={coverWidth}
-    height={coverHeight}
-  />
-
   <h1>{title}</h1>
 
   <div class="meta">
