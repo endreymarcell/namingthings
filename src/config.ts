44f708c4d5ac9d8b1c18413da429ba627fb3a8a9
Marcell Endrey <endrey.marcell@gmail.com>
2022-11-20 18:29:07 +0100

    chore: rework the template, add my own posts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "My personal website.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Naming things is hard";
+export const SITE_DESCRIPTION = "A blog about the hard parts of software engineering";
---
+++ src/pages/blog/drafts/waze.md
@@ -0,0 +1,7 @@
+---
+title: Burning questions about the UX of Waze
+date: 2022-11-02
+excerpt: TODO
+---
+
+TODO
